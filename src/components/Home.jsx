67459046d6829b7bf4d9b0f9253a39c643f46c79
Diwas Mainali <diwas.sok@gmail.com>
2024-06-08 01:13:27 +1000

    made a hom jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-10xl ml-2">Welcome to my portfolio! My name is Diwas</h1>
+        </div>
+    )
+}
+export default Home
