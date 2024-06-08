a94f128d616d7c9df26e783e74c6f80f7643b689
Diwas Mainali <diwas.sok@gmail.com>
2024-06-08 22:22:31 +1000

    fixed issue with mobile screen viewing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
         <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white">
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
                 <div>
-                   <h2 className="text-4xl sm:text-7xl font-bold text-white">Welcome to my porfolio!</h2> 
-                    <p className="text-gray-500 py-4 max-w-md">
+                <h2 className="text-4xl sm:text-4xl md:text-6xl font-bold text-white py-4">Welcome to my portfolio!</h2>                    
+                <p className="text-gray-500 py-4 max-w-md">
                     I am a passionate third-year computer science student at UNSW with a strong focus on front-end development. 
                     My expertise lies in creating engaging web experiences using HTML, CSS, JavaScript, React, and Tailwind CSS. 
                     As a freelance web developer, I take pride in delivering high-quality, responsive, and user-friendly websites to my clients. 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
     <div className="flex justify-between items-center w-full h-20
     px-4 text-white bg-black fixed">
         <div>
-            <h1 className="text-5xl ml-2">Diwas</h1>
+            <h1 className="text-5xl ml-2 font-bold">Diwas</h1>
         </div>
 
         <ul className="hidden md:flex">
