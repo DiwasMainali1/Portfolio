4a25f2227e4d9c7a3e68dfc93c50a21a82ae61c3
Diwas Mainali <diwas.sok@gmail.com>
2024-06-08 21:38:25 +1000

    Added images and made better layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-import profile from "../assets/hero.jpg";
-import { BsArrowRightSquareFill } from "react-icons/bs";
+import profile from "../assets/hero1.png";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
     return (
-        <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
+        <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white">
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
                 <div>
-                   <h2>Welcome to my porfolio!</h2> 
-                    <p>
-                        I enjoy working on web applications using technologies like React, HTML5 and Next JS. 
+                   <h2 className="text-4xl sm:text-7xl font-bold text-white">Welcome to my porfolio!</h2> 
+                    <p className="text-gray-500 py-4 max-w-md">
+                    I am a passionate third-year computer science student at UNSW with a strong focus on front-end development. 
+                    My expertise lies in creating engaging web experiences using HTML, CSS, JavaScript, React, and Tailwind CSS. 
+                    As a freelance web developer, I take pride in delivering high-quality, responsive, and user-friendly websites to my clients. 
                     </p>
                     <div>
-                        <button>
+                        <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
                             Portfolio
-                            <span>                        
-                                <BsArrowRightSquareFill />
+                            <span className="group-hover:rotate-90 duration-300">                        
+                                <MdKeyboardArrowRight />
                             </span>
                         </button>
                     </div>
                 </div>
                 <div>
                     <img src={profile} alt="my profile"
-                    className="rounded-2xl mx-auto w-2/3 md:w-full"/>
+                    className="rounded-2xl mx-auto w-2/3 md:w-2/3"/>
                 </div>
             </div>    
         </div>
