c790be4bb7d1fa5090997365ef04b291cbd455da
Diwas Mainali <diwas.sok@gmail.com>
2024-06-08 20:09:18 +1000

    Added profile photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,30 @@
+import React from "react";
+import profile from "../assets/hero.jpg";
+import { BsArrowRightSquareFill } from "react-icons/bs";
+
 const Home = () => {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="text-10xl ml-2">Welcome to my portfolio! My name is Diwas</h1>
+        <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
+            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+                <div>
+                   <h2>Welcome to my porfolio!</h2> 
+                    <p>
+                        I enjoy working on web applications using technologies like React, HTML5 and Next JS. 
+                    </p>
+                    <div>
+                        <button>
+                            Portfolio
+                            <span>                        
+                                <BsArrowRightSquareFill />
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    <img src={profile} alt="my profile"
+                    className="rounded-2xl mx-auto w-2/3 md:w-full"/>
+                </div>
+            </div>    
         </div>
     )
 }
